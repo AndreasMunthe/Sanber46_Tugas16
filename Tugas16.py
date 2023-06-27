af22ee3aa81d961e3f82054ecc6868ff30bb2d4a
AndreasMunthe <andreasmunthe04@gmail.com>
2023-06-27 14:18:05 +0700

    commit ketiga

--- Tugas16.py
+++ Tugas16.py
@@ -1,3 +1,5 @@
+
+import unittest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
@@ -6,11 +8,12 @@ from selenium.webdriver.common.keys import Keys
 import time
 from selenium.webdriver import Chrome
 from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 options=webdriver.ChromeOptions()
 options.add_experimental_option('detach',True)
 driver1=webdriver.Chrome(options=options)
-driver1.get("http://www.thetestingWorld.com/testings")
+driver1.get("https://www.saucedemo.com/")
 
 """
 #driver1.find_element(By.NAME, 'username').send_keys('iasdiasid')
@@ -48,8 +51,9 @@ action.context_click(square).perform()
 time.sleep(3)
 driver1.maximize_window()
 """
-#Maximize browser
 
+
+"""
 driver1.maximize_window()
 print("Succes maximize page")
 
@@ -76,6 +80,55 @@ driver1.find_element(By.NAME,'terms').click()
 obj=Select(driver1.find_element(By.NAME,'sex'))
 obj.select_by_index(1)
 obj.select_by_index(2)
+"""
 
+#Maximize browser
+driver1.maximize_window()
 
+#Login and Logout Succes (Scenario 1)
 
+# steps
+driver1.find_element(By.NAME,"user-name").send_keys("standard_user") # isi email
+time.sleep(1)
+driver1.find_element(By.NAME,"password").send_keys("secret_sauce") # isi password
+time.sleep(1)
+driver1.find_element(By.NAME, "login-button").click()
+time.sleep(1)
+driver1.find_element(By.ID, "react-burger-menu-btn").click()
+time.sleep(1)
+driver1.find_element(By.ID, "logout_sidebar_link").click()  
+
+time.sleep(5)
+
+#Failed login username is empty (Scenario 2)
+driver1.get("https://www.saucedemo.com/")
+time.sleep(1)
+driver1.find_element(By.NAME,"user-name").send_keys("") # email tidak terisi
+time.sleep(1)
+driver1.find_element(By.NAME,"password").send_keys("secret_sauce") # isi password
+time.sleep(1)
+driver1.find_element(By.NAME, "login-button").click()
+time.sleep(1)
+
+time.sleep(5)
+#Failed login Password is empty(Scenario 3)
+driver1.get("https://www.saucedemo.com/")
+time.sleep(1)
+driver1.find_element(By.NAME,"user-name").send_keys("standard_user") # email tidak terisi
+time.sleep(1)
+driver1.find_element(By.NAME,"password").send_keys("") # isi password
+time.sleep(1)
+driver1.find_element(By.NAME, "login-button").click()
+time.sleep(1)
+
+#Failed login Username & Password  is empty(Scenario 4)
+driver1.get("https://www.saucedemo.com/")
+time.sleep(1)
+driver1.find_element(By.NAME,"user-name").send_keys("") # email tidak terisi
+time.sleep(1)
+driver1.find_element(By.NAME,"password").send_keys("") # isi password
+time.sleep(1)
+driver1.find_element(By.NAME, "login-button").click()
+time.sleep(1)
+
+driver1.close()
